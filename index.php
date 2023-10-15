ae89876bf3413f0c8bc3c8013938eb23916f135c
yuramitrakhovich2 <84200792+yuramitrakhovich2@users.noreply.github.com>
2023-10-15 20:39:12 +0300

    commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+	<head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport"
+	        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	  <title>Говорушка</title>
+
+	  <link rel="stylesheet" href="csss/main1.css">
+
+	</head>
+
+		<main>
+	    <section class="hero" style="background-image: url('images/mainimage.jpeg')">
+	      <div class="container">
+	          <h1>Говорушка</h1>
+	      </div>
+	    </section>
+		</main>
+
+		<footer>
+	    <div class="container">
+	      <small class="footer-copyright">© 2023 Говорушка. Все права защищены</small>
+	    </div>
+		</footer>
+	</body>
+</html>
